5f925de892a7b6206c94c5f292ecdbcde5dcf6f1
stuffch1k <elizaveta.sineva@urfu.me>
2023-11-01 19:43:30 +0500

    changed db structure + topics description

--- src/models/model.py
+++ src/models/model.py
@@ -2,35 +2,58 @@ import databases
 import sqlalchemy
 from typing import Optional, Union, Dict
 import ormar
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], 'src'))
 from database import DATABASE_URL
 
 metadata = sqlalchemy.MetaData()
-
 database = databases.Database(DATABASE_URL)
 
-class Element(ormar.Model):
-    class Meta:
-        database = database
-        metadata = metadata
-    id: int = ormar.Integer(primary_key = True)
-    name: str = ormar.Text()
-    
 class Topic(ormar.Model):
     class Meta:
         database = database
         metadata = metadata
     id: int = ormar.Integer(primary_key = True)
     name: str = ormar.Text()
-    element: Optional[Union[Element, Dict]] = ormar.ForeignKey(Element, nullable = True)
+
+class Text(ormar.Model):
+    class Meta:
+        database = database
+        metadata = metadata
+    id: int = ormar.Integer(primary_key = True)
+    topic: Optional[Topic] = ormar.ForeignKey(Topic)
+    complexity: int = ormar.Integer()
+    text: str = ormar.Text()
 
 class Task(ormar.Model):
     class Meta:
         database = database
         metadata = metadata
     id: int = ormar.Integer(primary_key = True)
-    topic: Optional[Union[Topic, Dict]] = ormar.ForeignKey(Topic)
-    task: str = ormar.Text()
+    topic: Optional[Topic] = ormar.ForeignKey(Topic)
+    complexity: float = ormar.Float()
+    text: str = ormar.Text()
     answer: str = ormar.Text()
 
-    
+''' 
+Матрицы 10 средняя
+Метод Крамера 5 сложные
+
+
+0-9
+10-20 средняя
+21-30
+
+сумма матриц - 7
+3х3 - 3
+округление - 2
+
+формулировка задачи + размеры данных + окргуление
+'''
+
+# Замените элемент {element} на такой, чтобы детерминант был равен {determinant}
+
+
+  
 
