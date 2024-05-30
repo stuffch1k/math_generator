77a3fab78b1328d21037e5aa3bd44db1a4537610
stuffch1k <elizaveta.sineva@urfu.me>
2024-05-30 17:22:48 +0500

    Added cors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 import uvicorn 
 from api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://192.168.0.109",
+    "http://192.168.0.109:3000",
+    "https://overwrought-salt.surge.sh"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main():
